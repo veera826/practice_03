8d2275dddd1b9fc40612a0ef4b130ffcbc70c7d8
veera <veerababupanasa11@gmail.com>
2026-09-01 22:01:55 +0530

    'hjgk'

--- vara/src/main/java/com/veera/vara/App.java
+++ vara/src/main/java/com/veera/vara/App.java
@@ -6,7 +6,7 @@ package com.veera.vara;
 public class App {
     public static void main(String[] args) {
        
-    	String s="veerababu";
+    	String s="veerababuu";
     	
     	String s1="";
     	
