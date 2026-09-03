9570b270c4cbfb0a1ff503430f41330e226bc81d
veera <veerababupanasa11@gmail.com>
2026-09-03 12:16:12 +0530

    'screenshotAdded'

--- vara/src/test/java/com/veera/vara/Launch_Browser.java
+++ vara/src/test/java/com/veera/vara/Launch_Browser.java
@@ -1,12 +1,18 @@
 package com.veera.vara;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class Launch_Browser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
 		
@@ -18,7 +24,22 @@ public class Launch_Browser {
 		
 		Actions ac=new Actions(driver);
 		
-		 ac.moveByOffset(10,10).click().perform();
+		ac.moveByOffset(10,10).click().perform();
+		 
+		 
+		 
+		 TakesScreenshot ts=(TakesScreenshot)driver;
+		 
+		 File src=ts.getScreenshotAs(OutputType.FILE);
+		 
+		  File destfile=new File("./screenshots5/veera"+".png");
+		  
+		  FileUtils.copyFile(src, destfile);
+		  
+		  
+		  
+		 
+		 
 		
 
 	}
